7d7671b69d5992b817a5fce2f70da725b89f9e64
ArjayErasmo <erasmoarjhay4@gmail.com>
2023-01-12 17:34:42 +0800

    fb link
    for upload

--- app/Views/checkout.php
+++ app/Views/checkout.php
@@ -336,7 +336,7 @@
                             <div class="contact-info">
                                 <div class="social">
                                     <a href=""><i class="fab fa-twitter"></i></a>
-                                    <a href=""><i class="fab fa-facebook-f"></i></a>
+                                    <a href="https://www.facebook.com/profile.php?id=100076261489001"><i class="fab fa-facebook-f"></i></a>
                                     <a href=""><i class="fab fa-linkedin-in"></i></a>
                                     <a href=""><i class="fab fa-instagram"></i></a>
                                     <a href=""><i class="fab fa-youtube"></i></a>
