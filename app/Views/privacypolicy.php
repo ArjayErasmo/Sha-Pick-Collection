e24eb799a4b224800063a205ac6168116697b1b0
ArjayErasmo <erasmoarjhay4@gmail.com>
2023-01-18 01:02:48 +0800

    source code
    for upload

--- app/Views/privacypolicy.php
+++ app/Views/privacypolicy.php
@@ -7,6 +7,40 @@
     <title>Privacy and Policy</title>
 </head>
 <body>
-    <p>Hello this is Privacy and policy</p>
+    <p>Welcome to Sha-Pick Collection, a website that offers a wide range of products for purchase. 
+        We are committed to protecting the privacy of our customers and visitors to our website. This 
+        privacy policy sets out how we collect, use, and disclose personal information 
+        from our customers and visitors.</p>
+
+    <p><h3> Information We Collect</h3>
+We collect a variety of information from our customers and visitors, including but not limited to: <br> <br>
+
+Personal information, such as name, address, email address, and phone number <br>
+Payment information, such as credit card details <br>
+Information about your browsing and purchase history on our website <br></p>
+
+    <p><br><h3>How We Use Your Information</h3>
+We use the information we collect to provide you with a better shopping experience, including: <br>
+<br>
+Process and fulfill your orders <br>
+Send you information about your order, such as order confirmation and shipping details <br>
+Send you marketing and promotional materials, if you have opted in to receive such communications <br>
+Improve our website and product offerings</p>
+
+    <p><br><h3>Sharing of Your Information</h3>
+We do not sell, trade, or rent your personal information to third parties. We may, however, share your information with third parties in the following limited circumstances: <br>
+
+With service providers, such as payment processors, shipping companies, and data analytics providers, who help us operate our business and website <br>
+As required by law, such as to comply with a subpoena or other legal process <br>
+When we believe in good faith that disclosure is necessary to protect our rights, protect your safety or the safety of others, investigate fraud, or respond to a government request</p>
+
+    <p><br><h3>Security of Your Information</h3>
+We take reasonable steps to protect your personal information from unauthorized access, use, or disclosure. However, no method of transmission over the Internet, or method of electronic storage, is 100% secure. Therefore, we cannot guarantee its absolute security.</p>
+
+    <p><br><h3>Changes to this Policy</h3>
+We reserve the right to change this Policy at any time. Any changes to this Policy will be effective immediately upon posting the revised Policy on our website. By using our website, you agree to be bound by the then-current version of this Policy.</p>
+
+    <p><br><h3>Contact Information</h3>
+If you have any questions or concerns about this Policy, please contact us at [Contact email or phone number].</p>
 </body>
 </html>
