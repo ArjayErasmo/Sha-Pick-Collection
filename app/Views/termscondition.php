9e0d3889f01c46d8c8c356a0c6a2ef9c140c80d1
ArjayErasmo <erasmoarjhay4@gmail.com>
2023-01-18 23:32:36 +0800

    source code
    for upload

--- app/Views/termscondition.php
+++ app/Views/termscondition.php
@@ -5,15 +5,120 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Terms and Condition</title>
+    <style>
+        body{
+            margin: 0;
+            padding: 0;
+            background: linear-gradient(130deg, #e5cbcbca, #FF6F61) ;
+            min-height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        .terms-box{
+            max-width: 460px;
+            background-color: #111;
+            color: #fff;
+            font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+            padding: 60px 30px;
+
+        }
+        .terms-text{
+            padding: 0 20px;
+            height: 400px;
+            overflow-y: auto;
+            font-size: 14px;
+            font-weight: 500;
+            color: #f1f1f1;
+        }
+        .terms-text::-webkit-scrollbar{
+            width: 2px;
+            background-color: #282828;
+        }
+        .terms-text::-webkit-scrollbar-thumb{
+            background-color: #d13639;
+        }
+        .terms-text h2{
+            text-transform: uppercase;
+        }
+        .terms-box h4{
+            font-size: 13px;
+            text-align: center;
+            padding: 0 40px;
+        }
+        .terms-box h4 span{
+            color: #d13639;
+        }
+        .buttons{
+            display: flex;
+            padding: 0 20px;
+            justify-content: space-between;
+        }
+        .btn{
+            height: 50px;
+            width: calc(50% - 6px);
+            border: 0;
+            border-radius: 6px;
+            font-size: 19px;
+            font-weight: 500;
+            color: #fff;
+            transition: .3s linear;
+            cursor: pointer;
+        }
+        .red-btn{
+            background-color: #d13639;
+        }
+        .gray-btn{
+            background-color: #282828;
+        }
+        .btn:hover{
+            opacity: 0.6;
+        }
+    </style>
 </head>
 <body>
     <div class="terms-box">
         <div class="terms-text">
             <h2>Terms of Service</h2>
             <p>Last Edit: 01/18/2023</p>
-            <p>Greetings Users,</p>
-            
+            <p>Greetings Users,</p><br>
+            <p>.hjghffgftgvhghy</p>
+            <p>.hgfdygguygfgds</p>
+            <p>.hgfdygguygfgds</p>
+            <p>.hgfdygguygfgds</p>
+            <p>.hgfdygguygfgds</p>
+            <p>.hgfdygguygfgds</p>
+            <p>.hgfdygguygfgds</p>
+            <p>.hjghffgftgvhghy</p>
+            <p>.hjghffgftgvhghy</p>
+            <p>.hjghffgftgvhghy</p>
+            <p>.hjghffgftgvhghy</p>
+            <p>.hjghffgftgvhghy</p>
+            <p>.hjghffgftgvhghy</p>
+            <p>.hgfdygguygfgds</p>
+            <p>.hgfdygguygfgds</p>
+            <p>.hjghffgftgvhghy</p>
+            <p>.hjghffgftgvhghy</p>
+            <p>.hjghffgftgvhghy</p>
+            <p>.hjghffgftgvhghy</p>
+            <p>.hjghffgftgvhghy</p>
+            <p>.hjghffgftgvhghy</p>
+            <p>.hgfdygguygfgds</p>
+            <p>.hgfdygguygfgds</p>
+            <p>.hjghffgftgvhghy</p>
+            <p>.hjghffgftgvhghy</p>
+            <p>.hjghffgftgvhghy</p>
+            <p>.hjghffgftgvhghy</p>
+            <p>.hjghffgftgvhghy</p>
+            <p>.hjghffgftgvhghy</p>
+            <p>.hjghffgftgvhghy</p>
+            <p>.hjghffgftgvhghy</p>
         </div>
+        <h4>I Agree to the <span>Terms of Service</span>and I read the Privacy Notice.</h4>
+            <div class="buttons">
+                <button class="btn red-btn">Accept</button>
+                <button class="btn gray-btn">Decline</button>
+            </div>
     </div>
 </body>
 </html>
