f9b9c67ed11bd088bdc34aa2ceb551cfb846ae5c
ArjayErasmo <erasmoarjhay4@gmail.com>
2023-01-19 23:54:22 +0800

    source code
    for upload

--- app/Views/aboutUs.php
+++ app/Views/aboutUs.php
@@ -5,18 +5,70 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat&family=Poppins:wght@300&display=swap');
+        *{
+            margin: 0px;
+            padding: 0px;
+            box-sizing: border-box;
+            font-family: 'Poppins', sans-serif;
+        }
+        body{
+            width: 100%;
+            min-height: 100vh;
+            background-color: #1a192c;
+        }
+        .wrapper{
+            width: 100%;
+        }
+        .title h1{
+            color: #fff;
+            font-size: 46px;
+            font-weight: 700;
+            text-transform: uppercase;
+            text-align: center;
+            padding: 40px 0px;
+        }
+        .about{
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-top: 50px;
+            padding: 0px 80px;
+        }
+        .image-section{
+            width: 90%;
+            height: auto;
+            margin: auto;
+        }
+        .image-section img{
+            height: 280px;
+        }
+    </style>
 </head>
 <body>
-    <h1>Sha Pick Collection</h1>
-
-    <p>Sha Pick Collection is an online e-commerce system application wherein the owner
-is Ms. Shaina Marie J. Samarita, an online entrepreneur at a very young age. Her shop
-consists of different variety of products like shoes, dresses, shirts, shorts and
-many more. She founded her shop in the year 2016 during her high school days.
-
-Browse and buy in my shop where all my products are all authentic and good quality
-in an affordable prices
-
-Happy Shopping!</p>
+    <div class="wrapper">
+        <div class="title">
+            <h1>About Sha Pick Collection</h1>
+        </div>
+        <div class="about">
+            <div class="image-section">
+                <img src="#" alt="">
+            </div>
+            <article>
+                <h3>Sha Pick Collection</h3>
+                <p>Sha Pick Collection is an online e-commerce system application wherein the </p>
+                <p>owner is Ms. Shaina Marie J. Samarita, an online entrepreneur at a very young age. </p>
+                <p>Her shop consists of different variety of products like shoes, dresses, shirts, shorts and many more.</p> 
+                <p>She founded her shop in the year 2016 during her high school days. </p>
+                <p>Browse and buy in my shop where all my products are all authentic and good quality in an affordable prices </p> 
+                <p>Happy Shopping!</p>
+                <div class="button">
+                    <a href="">Learn More</a>
+                </div>
+            </article>
+        </div>
+    </div>
 </body>
 </html>
