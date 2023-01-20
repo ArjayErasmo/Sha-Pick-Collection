66e4c8c5195dbb75838d9b5c6af5c3b33d25baaa
Jean Diane Hogno <jheandianehogno15@gmail.com>
2023-01-20 23:06:19 +0700

    Update shippingpolicy.php

--- app/Views/shippingpolicy.php
+++ app/Views/shippingpolicy.php
@@ -1,12 +1,132 @@
-<!DOCTYPE html>
+<<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shipping Policy</title>
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat&family=Poppins:wght@300&display=swap');
+    *{
+        margin: 0px;
+        padding: 0px;
+        box-sizing: border-box;
+        font-family: 'Poppins', sans-serif;
+    }
+    body{
+        width: 100%;
+        min-height: 100vh;
+        background-color: #1a192c;
+    }
+    .wrapper{
+        width: 100%;
+    }
+    .title h1{
+        color: #fff;
+        font-size: 46px;
+        font-weight: 700;
+        text-transform: uppercase;
+        text-align: center;
+        padding: 40px 0px;
+    }
+    .about{
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 50px;
+        padding: 0px 80px;
+    }
+    .image-section{
+        width: 90%;
+        height: auto;
+        margin: auto;
+    }
+    .image-section img{
+        height: 280px;
+        width: 280px;
+        border-radius: 50%;
+        border: 1px solid #fff;
+        display: block;
+        margin: auto;
+    }
+    article{
+        width: 90%;
+    }
+    article h3{
+        font-size: 22px;
+        font-weight: 500;
+        color: #7d7d7d;
+    }
+    article p{
+        font-size: 17px;
+        color: #fff;
+        margin-top: 25px;
+    }
+    article .button{
+        margin-top: 35px;
+    }
+    article a{
+        color: #fff;
+        padding: 10px 25px;
+        font-size: 19px;
+        font-weight: 300;
+        text-decoration: none;
+        border: 1px solid #f3be00;
+        border-radius: 5px;
+    }
+    article a:hover{
+        background-color: #f3be00;
+        transition: 0.5s ease;
+    }
+    @media screen and (max-width: 768px){
+        .wrapper{
+            margin-top: 15px;
+        }
+        .title h1{
+            font-size: 28px;
+            text-align: center;
+        }
+    }
+    .row:after
+    {
+    content: "";
+    display: table;
+    clear: both;
+    }
+    .column
+    {
+    background: linear-gradient(130deg, #e5cbcbca, #FF6F61) ;
+    float: left;
+    width: 50%;
+    padding: 10px;
+    height: 500px;
+    text-align: center;
+    }
+    .center
+    {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+    }
+    </style>
 </head>
 <body>
-    <p>Hello this is shipping policy</p>
+<div class="wrapper">
+    <div class="title">
+        <h1>Sha Pick Collection Shipping Policy</h1>
+    </div>
+    <div class="row">
+    <div class="column">
+        <div img src="https://th.bing.com/th/id/OIF.PpWiN8LgRVEjB3UbOVRTjw?pid=ImgDet&rs=1" class="center"></div>
+        <!-- style="background-color:#bbb;" -->
+        <h2 text-align="center">delivery</h2>
+        <!-- on process editing -->
+    </div>
+    <div class="column">
+        <h2>Pick up</h2>
+        <p>on process editing</p>
+      </div>
 </body>
 </html>
