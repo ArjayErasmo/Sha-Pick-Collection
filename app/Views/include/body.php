2ca98201cbb8cb601c548f57dcbca63671114159
ArjayErasmo <erasmoarjhay4@gmail.com>
2023-01-20 23:52:48 +0800

    source code
    for upload

--- app/Views/include/body.php
+++ app/Views/include/body.php
@@ -172,12 +172,24 @@
         </div>
         <!-- Call to Action End -->       
         
-        <!-- Featured Product Start -->
-    
-        <!-- Featured Product End -->       
+               
         
         <!-- Newsletter Start -->
-        
+        <div class="newsletter">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h1>Subscribe Our Newsletter</h1>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form">
+                            <input type="email" value="Your email here">
+                            <button>Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <!-- Newsletter End -->        
         
         <!-- Recent Product Start -->
---
+++ app/Views/include/featureproducts.php
@@ -0,0 +1,47 @@
+<!-- Recent Product Start -->
+<div class="recent-product product">
+            <div class="container-fluid">
+                <div class="section-header">
+                    <h1>Featured Product</h1>
+                </div>
+                <div class="row align-items-center product-slider product-slider-4">
+
+                
+                    <?php foreach ($products as $pr): ?>
+                    <div class="col-lg-3">
+                        <div class="product-item">
+                            <div class="product-title">
+                                <a href="#">Product Name</a>
+                                <div class="ratting">
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                </div>
+                            </div>
+                            <div class="product-image">
+                                <a href="product-detail.html">
+                                    <img src="img/product-6.jpg" alt="Product Image">
+                                </a>
+                                <div class="product-action">
+                                    <a href="#"><i class="fa fa-cart-plus"></i></a>
+                                    <a href="#"><i class="fa fa-heart"></i></a>
+                                    <a href="#"><i class="fa fa-search"></i></a>
+                                </div>
+                            </div>
+                            <div class="product-price">
+                                <h3><span>$</span>99</h3>
+                                <a class="btn" href=""><i class="fa fa-shopping-cart"></i>Buy Now</a>
+                            </div>
+                        </div>
+                    </div>
+                    <?php endforeach; ?>
+
+
+                </div>
+            </div>
+        </div>
+        <!-- Recent Product End -->
+        
+        
