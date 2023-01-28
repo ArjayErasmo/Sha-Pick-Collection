b573fe1d5c0c76cd8339a38facebf29ce6d5c346
ArjayErasmo <erasmoarjhay4@gmail.com>
2023-01-29 02:15:35 +0800

    Payment Policy Content
    for upload

--- app/Views/paymentpolicy.php
+++ app/Views/paymentpolicy.php
@@ -5,8 +5,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Payment Policy</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat&family=Poppins:wght@300&display=swap');
+        *{
+            margin: 0px;
+            padding: 0px;
+            box-sizing: border-box;
+            font-family: 'Poppins', sans-serif;
+        }
+        body{
+            align-items: center;
+            width: 100%;
+            min-height: 100vh;
+            background: linear-gradient(130deg, #e5cbcbca, #FF6F61) ;
+        }
+        p{
+            
+            font-size: 14px;
+            font-weight: 500;
+            color: #000;
+        }
+    </style>
 </head>
 <body>
-    <p>Hello this is Payment Policy</p>
+    <p>Welcome to our Payment Policy, where we outline the different methods of payment available for purchases on our website, including manual payment with the seller and online payment.</p>
+
+    <p><h3> Payment Policy</h3>
+    <br>
+    <p>1. Payment for purchases on our website can be made either through manual payment with the seller or through online payment using Gcash.</p>
+    <p>2. Manual payment with the seller can be made by cash or money order at the time of pick-up or delivery.</p>
+    <p>3. Online payment using Gcash can be made by following the instructions provided at checkout. Customers will need to have a Gcash account to complete the transaction.</p>
+    <p>4. All payments must be made in full before the item(s) are shipped or delivered.</p>
+    <p>5. We reserve the right to cancel any order that is not paid for within the specified timeframe.</p>
+    <br>
+        If you have any questions or concerns about this Policy, please contact us at shainamariesamarita14@gmail.com or 0905 474 2754.</p>
 </body>
 </html>
