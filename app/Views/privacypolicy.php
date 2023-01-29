b2d79dca31c8cf03eb0ba6ea434679860bd29881
Jean Diane Hogno <jheandianehogno15@gmail.com>
2023-01-29 23:53:03 +0700

    Update privacypolicy.php

--- app/Views/privacypolicy.php
+++ app/Views/privacypolicy.php
@@ -18,22 +18,45 @@
             min-height: 100vh;
             background: linear-gradient(130deg, #e5cbcbca, #FF6F61) ;
         }
+        .wrapper{
+        width: 100%;
+        }
+        .title{
+            color: black;
+            font-size: 20px;
+            font-weight: 700;
+            text-transform: uppercase;
+            text-align: center;
+            padding: 40px 0px;
+        }
+        .center
+        {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 50%;
+        }
     </style>
 </head>
 <body>
+    <div class="wrapper">
+        <div class="title">
+            <h1>Sha Pick Collection Shipping Policy</h1>
+        </div>
+        <div style = "text-align: center;">
     <p>Welcome to Sha-Pick Collection, a website that offers a wide range of products for purchase. 
         We are committed to protecting the privacy of our customers and visitors to our website. This 
         privacy policy sets out how we collect, use, and disclose personal information 
-        from our customers and visitors.</p>
-
-    <p><h3> Information We Collect</h3>
+        from our customers and visitors.<br></p>
+        <br>
+    <p><h3 style = "text-align: center;"> Information We Collect</h3>
 We collect a variety of information from our customers and visitors, including but not limited to: <br> <br>
 
 Personal information, such as name, address, email address, and phone number <br>
 Payment information, such as credit card details <br>
 Information about your browsing and purchase history on our website <br></p>
 
-    <p><br><h3>How We Use Your Information</h3>
+    <p><br><h3 style = "text-align: center;">How We Use Your Information</h3>
 We use the information we collect to provide you with a better shopping experience, including: <br>
 <br>
 Process and fulfill your orders <br>
@@ -41,20 +64,21 @@ Send you information about your order, such as order confirmation and shipping d
 Send you marketing and promotional materials, if you have opted in to receive such communications <br>
 Improve our website and product offerings</p>
 
-    <p><br><h3>Sharing of Your Information</h3>
+    <p><br><h3 style = "text-align: center;">Sharing of Your Information</h3>
 We do not sell, trade, or rent your personal information to third parties. We may, however, share your information with third parties in the following limited circumstances: <br>
 
 With service providers, such as payment processors, shipping companies, and data analytics providers, who help us operate our business and website <br>
 As required by law, such as to comply with a subpoena or other legal process <br>
 When we believe in good faith that disclosure is necessary to protect our rights, protect your safety or the safety of others, investigate fraud, or respond to a government request</p>
 
-    <p><br><h3>Security of Your Information</h3>
+    <p><br><h3 style = "text-align: center;">Security of Your Information</h3>
 We take reasonable steps to protect your personal information from unauthorized access, use, or disclosure. However, no method of transmission over the Internet, or method of electronic storage, is 100% secure. Therefore, we cannot guarantee its absolute security.</p>
 
-    <p><br><h3>Changes to this Policy</h3>
+    <p><br><h3 style = "text-align: center;">Changes to this Policy</h3>
 We reserve the right to change this Policy at any time. Any changes to this Policy will be effective immediately upon posting the revised Policy on our website. By using our website, you agree to be bound by the then-current version of this Policy.</p>
 
-    <p><br><h3>Contact Information</h3>
+    <p><br><h3 style = "text-align: center;">Contact Information</h3>
 If you have any questions or concerns about this Policy, please contact us at shainamariesamarita14@gmail.com or 0905 474 2754.</p>
+    </div>
 </body>
 </html>
